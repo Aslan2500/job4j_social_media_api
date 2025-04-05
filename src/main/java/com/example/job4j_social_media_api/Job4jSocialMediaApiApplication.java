15fa46aa0b7193ba813c7a90f871a10bb234fbd9
Aslan2500 <aslanmagamaev@gmail.com>
2025-04-05 19:13:14 +0300

    spring init

---
+++ src/main/java/com/example/job4j_social_media_api/Job4jSocialMediaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.job4j_social_media_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jSocialMediaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jSocialMediaApiApplication.class, args);
+    }
+
+}
